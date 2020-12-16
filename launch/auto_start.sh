dc44e9b6d45ab23b1852a8a79058fa3e1c7f0a41
shangjie-li <1814481684@qq.com>
2020-12-16 21:07:08 +0800

    modified parameter of launch file

--- launch/auto_start.sh
+++ launch/auto_start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 source /opt/ros/melodic/setup.bash
-source /home/seucar/wendao/sweeper/ros_ws/devel/setup.bash
+source ~/wendao/sweeper/ros_ws/devel/setup.bash
 
-roslaunch /home/seucar/wendao/sweeper/ros_ws/src/my_image_publisher/launch/img_publisher.launch camera_id:=/dev/camera_r &
+roslaunch ~/wendao/sweeper/ros_ws/src/my_image_publisher/launch/img_publisher.launch camera_id:=/dev/camera_r &
 sleep 5
 
-roslaunch /home/seucar/wendao/sweeper/ros_ws/src/transmiter/launch/transmiter.launch &
+roslaunch ~/wendao/sweeper/ros_ws/src/transmiter/launch/transmiter.launch &
 sleep 5
 
 #!/usr/bin/env python3
 
-cd /home/seucar/wendao/sweeper/ros_ws/src/sweeper_detection/scripts/
+cd ~/wendao/sweeper/ros_ws/src/sweeper_detection/scripts/
 rosparam load param.yaml
 python3 detection_v7.py
 sleep 5
