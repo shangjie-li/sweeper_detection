bec3db5f2ce5e273efc27f086c8306a22ce613d2
shangjie-li <1814481684@qq.com>
2020-12-16 21:09:49 +0800

    first version

---
+++ launch/auto_start_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source /opt/ros/kinetic/setup.bash
+source ~/wendao/sweeper/ros_ws/devel/setup.bash
+
+#!/usr/bin/env python3
+
+cd ~/wendao/sweeper/ros_ws/src/sweeper_detection/scripts/
+rosparam load param.yaml
+python3 detection_v7.py
